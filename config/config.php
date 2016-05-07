8f839b4649f3c3947867328953df3a97315a3dd3
Hunter Hardman <t3ntman@gmail.com>
2016-05-07 11:41:51 -0600

    Removed extra hyphen in comment

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
   # Change if webroot isn't /var/www/html - Forward slash at the end is necessary and must not be ommitted
   $webrootPath = "/var/www/html/";
 
-  # Uploads directory - - Forward slash at the end is necessary and must not be ommitted
+  # Uploads directory - Forward slash at the end is necessary and must not be ommitted
   $uploadsPath = $webrootPath . "uploads/";
 
   # Don't change
