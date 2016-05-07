05bad47605de8e078056b73cd1b3a865dc9f06cf
Hunter Hardman <t3ntman@gmail.com>
2016-05-07 11:41:03 -0600

    Modified RAT configuration file

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
   # RAT configuration file
 
-  # Change if webroot isn't /var/www/html
-  $webrootPath = "/var/www/html";
+  # Change if webroot isn't /var/www/html - Forward slash at the end is necessary and must not be ommitted
+  $webrootPath = "/var/www/html/";
 
-  # Uploads directory
-  $uploadsPath = "/var/www/html/uploads/";
+  # Uploads directory - - Forward slash at the end is necessary and must not be ommitted
+  $uploadsPath = $webrootPath . "uploads/";
 
   # Don't change
   $dbHost = "127.0.0.1";
