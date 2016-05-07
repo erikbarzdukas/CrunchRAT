f6a1f259e751be42e1c6e701001e875eacd8d4a3
Hunter Hardman <t3ntman@gmail.com>
2016-05-07 11:10:20 -0600

    Added RAT configuration file

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+  # RAT configuration file
+
+  # Change if webroot isn't /var/www/html
+  $webrootPath = "/var/www/html";
+
+  # Uploads directory
+  $uploadsPath = "/var/www/html/uploads/";
+
+  # Don't change
+  $dbHost = "127.0.0.1";
+
+  # Don't change
+  $dbName = "RAT";
+
+  # Don't change
+  $dbUser = "root";
+
+  # Change
+  $dbPass = "root";
+?>
