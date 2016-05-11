9465dd1b3c9532483c77bf3075e87b5fb6745c0f
Hunter Hardman <t3ntman@gmail.com>
2016-05-11 09:54:23 -0600

    Removed old files

--- database_setup.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE RAT;
-USE RAT;
-CREATE TABLE users (username VARCHAR(50), password VARCHAR(50));
-INSERT INTO users (username, password) values ("admin", "changeme");
-CREATE TABLE hosts (hostname VARCHAR(100), date VARCHAR(100), os VARCHAR(200), architecture VARCHAR(3));
-CREATE TABLE tasks (id INT(16) AUTO_INCREMENT, hostname VARCHAR(100), action VARCHAR(20), secondary TEXT(65535), PRIMARY KEY (id));
-CREATE TABLE output (id INT(16) AUTO_INCREMENT, hostname VARCHAR(100), action VARCHAR(20), secondary TEXT(65535), stdout TEXT(65535), stderr TEXT(65535), status VARCHAR(1), PRIMARY KEY (id));
--- uploadSubmit.php
+++
@@ -1,5 +0,0 @@
-<?php
-  # Redirects back to upload.php
-  # Necessary to clear file upload POST data
-  header("Location: upload.php");
-?>
