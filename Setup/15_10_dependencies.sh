48044cd722489d2f965a2ee6ef9fccc14a90f028
Hunter Hardman <t3ntman@gmail.com>
2016-05-31 20:55:42 -0700

    Fixed weird issue with single quotes

--- Setup/15_10_dependencies.sh
+++ Setup/15_10_dependencies.sh
@@ -31,6 +31,6 @@ else
   chown www-data:www-data /var/www/html/uploads
   
   # Uses sed to increase the maximum POST size to 500 megs in php.ini
-  sed -i -e ’s/post_max_size = 8M/post_max_size = 500M/g’ /etc/php5/apache2/php.ini
-  sed -i -e ’s/upload_max_filesize = 2M/upload_max_filesize = 2M/g’ /etc/php5/apache2/php.ini
+  sed -i -e 's/post_max_size = 8M/post_max_size = 500M/g' /etc/php5/apache2/php.ini
+  sed -i -e 's/upload_max_filesize = 2M/upload_max_filesize = 500M/g' /etc/php5/apache2/php.ini
 fi
