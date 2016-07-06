667dfa63f89385b7cfda711ea709fe9b6b1aa566
Hunter Hardman <t3ntman@gmail.com>
2016-07-05 19:10:37 -0600

    Updated database_setup.sh with regards to stdout
    No longer using stdout and stderr columns. Just using single output column now.

--- Setup/database_setup.sh
+++ Setup/database_setup.sh
@@ -13,7 +13,7 @@ statement="$statement CREATE TABLE users (username VARCHAR(50), password VARCHAR
 statement="$statement INSERT INTO users (username, password) values ('admin', 'changeme'); ";
 statement="$statement CREATE TABLE hosts (hostname VARCHAR(100), date VARCHAR(100), os VARCHAR(200), architecture VARCHAR(3), pid VARCHAR(10), pfilename VARCHAR(260)); ";
 statement="$statement CREATE TABLE tasks (id INT(16) AUTO_INCREMENT, user VARCHAR(50), hostname VARCHAR(100), action VARCHAR(20), secondary TEXT(65535), PRIMARY KEY (id)); ";
-statement="$statement CREATE TABLE output (id INT(16) AUTO_INCREMENT, user VARCHAR(50), hostname VARCHAR(100), action VARCHAR(20), secondary TEXT(65535), stdout TEXT(65535), stderr TEXT(65535), status VARCHAR(1), PRIMARY KEY (id)); ";
+statement="$statement CREATE TABLE output (id INT(16) AUTO_INCREMENT, user VARCHAR(50), hostname VARCHAR(100), action VARCHAR(20), secondary TEXT(65535), output TEXT(65535), status VARCHAR(1), PRIMARY KEY (id)); ";
 statement="$statement CREATE TABLE chat (user VARCHAR(50), date VARCHAR(100), message TEXT(65535)); ";
 
 mysql -u"root" -p"$rootPassword" -e"$statement"
