fb3d74206439c1206914ca7632664a039b4011eb
root <t3ntman@gmail.com>
2016-08-25 11:41:45 -0600

    Added in CDN links and changed indenting

--- Server/403.php
+++ Server/403.php
@@ -1,21 +1,24 @@
 <!doctype html>
 <html lang="en">
-  <head> <!-- Start of header -->
-    <meta charset="utf-8">
-    <title>CrunchRAT</title>
-    <link href="bootstrap/css/bootstrap.css" rel="stylesheet"> <!-- Bootstrap CSS -->
-    <style>
-      body 
-      {
-        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-      }
-    </style>
-    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-  </head> <!-- End of header -->
+    <head> <!-- Start of header -->
+        <meta charset="utf-8">
+        <title>CrunchRAT</title>
+        <!-- CDN links -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-1.12.3.js"></script>
+        <style>
+            body 
+            {
+                padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+            }
+        </style>
+    </head> <!-- End of header -->
 
-  <body> <!-- Start of body -->
-    <div class="container"> <!-- Start of container -->        
-      <p>You are not authorized to view this page. Return to the login page <a href="login.php">here</a>.</p>
-    </div> <!-- End of container -->
-  </body> <!-- End of body -->
+    <body> <!-- Start of body -->
+        <div class="container"> <!-- Start of main body container -->        
+            <p>You are not authorized to view this page. Return to the login page <a href="login.php">here</a>.</p>
+        </div> <!-- End of main body container -->
+    </body> <!-- End of body -->
 </html>
