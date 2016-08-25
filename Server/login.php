03a19a4e26b738efddf3e8db68638a748f286dee
root <t3ntman@gmail.com>
2016-08-25 11:38:26 -0600

    Removed unecessary dataTables CDN links

--- Server/login.php
+++ Server/login.php
@@ -18,13 +18,9 @@
         <title>CrunchRAT</title>
         <!-- CDN links -->
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
         <script src="https://code.jquery.com/jquery-1.12.3.js"></script>
-        <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
     </head> <!-- End of header -->
 
     <body> <!-- Start of body -->
